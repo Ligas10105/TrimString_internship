22abf2b1c21f7b2a06a340884a298c934d58d50a
ligas10105 <ligass22@yahoo.com>
2023-04-19 01:27:50 +0200

    old version od program

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,14 @@ std::string textTrim(std::string text, Predicate predicate){
 }
 
 bool isWhitespace(char whitespace){
-    return whitespace == ' ' || whitespace == '\n' || whitespace == '\t';
+    return whitespace == ' ' || whitespace == '\t' || whitespace == '\n';
 }
+
+bool isDigit(char digit){
+    return digit >= '0' && digit <= '9';
+}
+
+
 
 
 int main() {
@@ -28,7 +34,15 @@ int main() {
     std::string trimmed_text = textTrim(text, isWhitespace);
     std::cout << trimmed_text << std::endl;
 
+    std::string text_1 = "12345Novomatic5678";
+    std::string trimmed_text_1 = textTrim(text_1, isDigit);
+    std::cout << trimmed_text_1 << std::endl;
+
+
+
 }
+
+
 
 
 
